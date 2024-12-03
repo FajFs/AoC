329fd57ef049088ad2b56a803abf8c44ebc8a24b
Felix Andersson Johansson <felix.andersson.johansson@prevas.se>
2024-12-03 07:24:48 +0100

    day03

--- src/Year2024/Day03.cs
+++ src/Year2024/Day03.cs
@@ -7,18 +7,45 @@ public partial class Day03(
     private readonly ILogger<Day03> _logger = _logger ?? throw new ArgumentNullException(nameof(_logger));
     private readonly AdventOfCodeClient _client = _client ?? throw new ArgumentNullException(nameof(_client));
 
+    [GeneratedRegex(@"mul\((\d+),(\d+)\)", RegexOptions.Compiled)]
+    public partial Regex MultiplicationRegex();
+
+    [GeneratedRegex(@"do\(\)|don't\(\)|mul\((\d+),(\d+)\)", RegexOptions.Compiled)]
+    public partial Regex DoDontOrMultiplicationRegex();
+
+    private static readonly string Do = "do()";
+    private static readonly string Dont = "don't()";
+
     public async Task SolvePart1()
     {
         var input = await _client.GetInputAsync(2024, 3);
+        var result = input.Split('\n')
+            .SelectMany(line => MultiplicationRegex().Matches(line)
+                .Select(match => int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value)))
+            .Sum();
 
-        //_logger.LogInformation("{part}: {result}", nameof(SolvePart1), result);
+        _logger.LogInformation("{part}: {result}", nameof(SolvePart1), result);
     }
 
 
     public async Task SolvePart2()
     {
         var input = await _client.GetInputAsync(2024, 3);
+        var doDontOrMuls = input.Split('\n')
+            .SelectMany(line => DoDontOrMultiplicationRegex().Matches(line));
 
-        //_logger.LogInformation("{part}: {result}", nameof(SolvePart2), result);
+        var canDo = true;
+        var result = 0;
+        foreach (var match in doDontOrMuls)
+        {
+            if (match.Value == Do)
+                canDo = true;
+            else if (match.Value == Dont)
+                canDo = false;
+            else if (canDo)
+                result += int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value);
+        }
+
+        _logger.LogInformation("{part}: {result}", nameof(SolvePart2), result);
     }
 }
